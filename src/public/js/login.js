de7e6b86d27a5651cdda20bf2622178400457430
Katia Villavicencio <125634854+KatiaVillavicencio@users.noreply.github.com>
2023-12-04 12:28:21 -0500

    user.js

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -4,24 +4,26 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     const email = document.querySelector("#email").value
     const password = document.querySelector("#password").value
 
-    const response = await fetch("/login",{
+    const response = await fetch("/login", {
         method: "POST",
-        body: JSON.stringify({email, password}), 
+        body: JSON.stringify({ email, password }),
         headers: {
-            "Content-Type": "application/json"
+          "Content-Type": "application/json",
+        },
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("token", data.token);        
+        if (data.token && data.user.rol === 'admin') 
+        {
+            //Acceso administrador
+            window.location.href = '/admin';
+        } else if (data.token && data.user.rol === 'usuario') {
+          //Acceso Usuario
+            window.location.href = '/current';
         }
-    })
-    if(response.ok){
-        const data = await response.json()
-        localStorage.setItem("token", data.token);
-
-if (data.token && data.user.rol == "admin"){}
-else if (data.token && data.user.rol === "usuario"){
-    window.location.href= '/current';
-}
-
-
-    }else{
-        console.error("Error en el inicio de sesion")
-    }
+      } else {
+        console.error("Error en el inicio de sesión");
+      }
 })
